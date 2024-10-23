4a71ebdb061b577cf8f2b8d5febda4262964ec73
s_hatta <hatta.shogo@gmail.com>
2024-10-23 22:14:08 +0900

    add: blade and css

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <a class="header__logo" href="/">
+        Contact Form
+      </a>
+    </div>
+  </header>
+
+  <main>
+    <div class="thanks__content">
+      <div class="thanks__heading">
+        <h2>お問い合わせありがとうございます</h2>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
