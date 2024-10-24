d26e8b85d2c09a1aebf7364142ebfb00303b8c43
s_hatta <hatta.shogo@gmail.com>
2024-10-24 21:37:47 +0900

    chage: use app.blade

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -7,25 +7,19 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
   <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
-  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
+
 </head>
 
-<body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
-    </div>
-  </header>
+@extends('layouts.app')
 
-  <main>
-    <div class="thanks__content">
-      <div class="thanks__heading">
-        <h2>お問い合わせありがとうございます</h2>
-      </div>
-    </div>
-  </main>
-</body>
+@section('css')
+  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
+@endsection
 
-</html>
+@section('content')
+  <div class="thanks__content">
+    <div class="thanks__heading">
+      <h2>お問い合わせありがとうございます</h2>
+    </div>
+  </div>
+@endsection
